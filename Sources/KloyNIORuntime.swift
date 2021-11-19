d0762ba044c82942b12d85bbbb06268754ba099b
DiedeClaessens <87484823+DiedeClaessens@users.noreply.github.com>
2021-11-19 16:41:45 +0100

    Fixed formatting

--- Sources/KloyNIORuntime.swift
+++ Sources/KloyNIORuntime.swift
@@ -2,91 +2,89 @@ import Foundation
 #if canImport(FoundationNetworking)
 import FoundationNetworking
 #endif
+import Core
 import NIO
 import NIOHTTP1
 import NIOHTTPCompression
-import Core
 
-
-    
 @available(macOS 12.0.0, *)
-public func run(server : Server, baseUrl: URL, eventLoopGroup: EventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount), port: Int = 8080){
+public func run(server: Server, baseUrl: URL, eventLoopGroup: EventLoopGroup = MultiThreadedEventLoopGroup(numberOfThreads: System.coreCount), port: Int = 8080) {
     do {
-      let reuseAddrOpt = ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR)
-      let bootstrap = ServerBootstrap(group: eventLoopGroup)
-        //Set up the Server Chanel
-        .serverChannelOption(ChannelOptions.backlog, value: 256)
-        .serverChannelOption(reuseAddrOpt, value: 1)
-        //Set up closure that will be used to initialise child channels (when a connection is accepted to our server)
-        .childChannelInitializer { channel in
-          channel.pipeline.configureHTTPServerPipeline().flatMap {
-              //Add handler
-            let handlers: [ChannelHandler] = [KloyHandler(for: server)]
-            return channel.pipeline.addHandlers(handlers, position: .last)
-          }
-        }
-        .childChannelOption(ChannelOptions.socket(IPPROTO_TCP, TCP_NODELAY), value: 1)
-        .childChannelOption(reuseAddrOpt, value: 1)
-        .childChannelOption(ChannelOptions.maxMessagesPerRead, value: 1)
+        let reuseAddrOpt = ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR)
+        let bootstrap = ServerBootstrap(group: eventLoopGroup)
+            // Set up the Server Chanel
+            .serverChannelOption(ChannelOptions.backlog, value: 256)
+            .serverChannelOption(reuseAddrOpt, value: 1)
+            // Set up closure that will be used to initialise child channels (when a connection is accepted to our server)
+            .childChannelInitializer { channel in
+                channel.pipeline.configureHTTPServerPipeline().flatMap {
+                    // Add handler
+                    let handlers: [ChannelHandler] = [KloyHandler(for: server)]
+                    return channel.pipeline.addHandlers(handlers, position: .last)
+                }
+            }
+            .childChannelOption(ChannelOptions.socket(IPPROTO_TCP, TCP_NODELAY), value: 1)
+            .childChannelOption(reuseAddrOpt, value: 1)
+            .childChannelOption(ChannelOptions.maxMessagesPerRead, value: 1)
 
-      let host = "0.0.0.0"
-      let serverChannel = try bootstrap.bind(host: host, port: port).wait()
-      print("Listening on \(host):\(port)...")
-      try serverChannel.closeFuture.wait()
-      try eventLoopGroup.syncShutdownGracefully()
+        let host = "0.0.0.0"
+        let serverChannel = try bootstrap.bind(host: host, port: port).wait()
+        print("Listening on \(host):\(port)...")
+        try serverChannel.closeFuture.wait()
+        try eventLoopGroup.syncShutdownGracefully()
     } catch {
-      fatalError(error.localizedDescription)
+        fatalError(error.localizedDescription)
     }
 }
 
 @available(macOS 12.0.0, *)
-private final class KloyHandler: ChannelInboundHandler{
+private final class KloyHandler: ChannelInboundHandler {
     typealias InboundIn = HTTPServerRequestPart // HTTPPart<HTTPRequestHead, ByteBuffer>
     let server: Server
     var kloyMethod: Core.HTTPMethod?
     var kloyHeaders: [Core.Header]?
-    var kloyUri:String?
+    var kloyUri: String?
     var kloyHttpVersion: Core.HTTPVersion?
     var request: Core.Request?
-    
+
     init(for server: Server) {
         self.server = server
         self.kloyHeaders = []
     }
-    
-    func channelRead(context: ChannelHandlerContext, data: NIOAny) async{
-        let reqPart  = self.unwrapInboundIn(data)
-        
+
+    func channelRead(context: ChannelHandlerContext, data: NIOAny) async {
+        let reqPart = self.unwrapInboundIn(data)
+
         switch reqPart {
         case let .head(header):
             self.kloyMethod = .init(rawValue: header.method.rawValue.lowercased())!
             let headers = header.headers.reduce(into: [:]) { $0[$1.name] = $1.value }
-            headers.forEach{
+            headers.forEach {
                 key, value in
                 self.kloyHeaders?.append(.init(name: key, value: value))
             }
-            //header.version.
+            // header.version.
             self.kloyUri = header.uri
             self.kloyHttpVersion = .oneOne
         case var .body(bodyPart):
             var data: Data = .init()
             let bytes = bodyPart.readBytes(length: bodyPart.readableBytes)
-            
-            bytes?.forEach{
+
+            bytes?.forEach {
                 data.append(.init($0))
             }
             self.request = .init(method: self.kloyMethod!, headers: self.kloyHeaders!, uri: self.kloyUri!, version: self.kloyHttpVersion!, body: .init(payload: data))
         case .end:
-            guard let req = self.request else{
+            guard let req = self.request else {
                 return
             }
-            
+
             let res = await self.server.process(request: req)
-            
+
             let head = HTTPResponseHead(
-              version: .init(major: 1, minor: 1),
-              status: .init(statusCode: res.status.code),
-              headers: .init(res.headers.map { ($0.name, $0.value) })
+                version: .init(major: 1, minor: 1),
+                status: .init(statusCode: res.status.code),
+                headers: .init(res.headers.map { ($0.name, $0.value) })
             )
             context.channel.write(HTTPServerResponsePart.head(head), promise: nil)
 
@@ -95,15 +93,8 @@ private final class KloyHandler: ChannelInboundHandler{
             context.channel.write(HTTPServerResponsePart.body(.byteBuffer(buffer)), promise: nil)
 
             _ = context.channel.writeAndFlush(HTTPServerResponsePart.end(nil)).flatMap {
-              context.channel.close()
+                context.channel.close()
             }
         }
-        
-        
-        
-        
-        //self.server.process(request: )
     }
 }
-
-//
