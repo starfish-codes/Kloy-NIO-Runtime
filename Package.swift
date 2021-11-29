8a3f0112b46b022c89abdbe77e9ee1d3aaddb3bc
DiedeClaessens <87484823+DiedeClaessens@users.noreply.github.com>
2021-11-29 14:48:46 +0100

    Updated kloy-core dependecy

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
-        .package(url: "https://github.com/starfish-codes/Kloy-Core", .branch("more-public")),
+        .package(url: "https://github.com/starfish-codes/Kloy-Core", .branch("main")),
 
     ],
     targets: [
